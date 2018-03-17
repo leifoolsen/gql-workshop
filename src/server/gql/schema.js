43e17d719d596eb1a2a27443c1450857bcd991fa
Olsen, Leif Oddvar <extloo@brreg.no>
2018-03-17 14:38:51 +0100

    Lagt til bookByTitle query Lagt til addBook muation

--- src/server/gql/schema.js
+++ src/server/gql/schema.js
@@ -10,23 +10,42 @@ const books = [
     title: 'Jurassic Park',
     author: 'Michael Crichton',
   },
+  {
+    title: 'The awesome',
+    author: 'Knowit Sør',
+  },
 ];
 
 // The GraphQL schema in string form
 const typeDefs = `
   type Query { 
     books: [Book] 
+    booksByTitle(title: String!): Book
+  }
+  type Mutation {
+    addBook(title: String!, author: String!): Book
   }
   type Book { 
     title: String, 
-    author: String 
+    author: String,
   }
 `;
 
 // The resolvers
 const resolvers = {
   Query: {
-    books: () => books
+    books: () => books,
+    booksByTitle: (root, {title}) => books.find((t) => t.title === title)
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const newBook = {
+        title: args.title,
+        author: args.author
+      };
+      books.push(newBook);
+      return newBook;
+    },
   },
 };
 
